c85483c9e0487688bef05e2f2292900c472eb9f7
David Ulrich <dulrich@c2r2p2.42vienna.com>
2023-09-20 14:26:14 +0200

    updating

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,11 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 10:57:57 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/20 13:58:58 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/20 14:25:54 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_prinft.h"
+#include "ft_printf.h"
 
 int ft_printf(const char *, ...)
 {
