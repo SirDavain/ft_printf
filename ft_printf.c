edf6fbaaefd0f8dabbeec04cb3f3e4f039689e0c
David Ulrich <dulrich@c2r3p3.42vienna.com>
2023-09-25 14:07:59 +0200

    hexadecimal conversion should be finished

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dulrich <dulrich@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 10:57:57 by dulrich           #+#    #+#             */
-/*   Updated: 2023/09/25 12:34:45 by dulrich          ###   ########.fr       */
+/*   Updated: 2023/09/25 14:07:35 by dulrich          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,6 +90,51 @@ char	*ft_unsigned_itoa(unsigned int n)
 	return (nbr);
 }
 
+int	ft_hex_len(unsigned int n)
+{
+	int	len;
+
+	len = 0;
+	while (n != 0)
+	{
+		len++;
+		n /= 16;
+	}
+	return (len);
+}
+
+void	ft_puthex(unsigned int n, const char format)
+{
+	if (n >= 16)
+	{
+		ft_puthex(n / 16, format);
+		ft_puthex(n % 16, format);
+	}
+	else
+	{
+		if (n <= 9)
+		{
+			ft_putchar((n + '0'));
+		}
+		else
+		{
+			if (format == 'X')
+				ft_putchar(n - 10 + 'A');
+			else if (format == 'x')
+				ft_putchar(n - 10 + 'a');
+		}
+	}
+}
+
+int	ft_printhex(unsigned int n, const char format)
+{
+	if (n == 0)
+		return (write(1, "0", 1));
+	else
+		ft_puthex(n, format);
+	return (ft_hex_len(n));
+}
+
 void	*ft_putptr()
 {
 	
@@ -108,7 +153,7 @@ int	ft_check_format(va_list args, const char format)
 	else if (format == 'd' || format == 'i')
 		print_len += ft_putnbr(va_arg(args, int));
 	else if (format == 'u')
-		print_len += ft_putun_nbr(va_arg(args, unsigned int));
+		print_len += ft_unsigned_putnbr(va_arg(args, unsigned int));
 	else if (format == 'x')
 		print_len += ft_puthex_lower(va_arg(args, int));
 	else if (format == 'X')
@@ -125,7 +170,7 @@ int ft_printf(const char *str, ...)
 	int		print_len;
 
 	i = 0;
-	if (!str[i])
+	if (!str)
 		return (-1);
 	va_start(args, str);
 	while (str[i])
@@ -133,6 +178,7 @@ int ft_printf(const char *str, ...)
 		if (str[i] == '%')
 		{
 			print_len += ft_check_format(args, str[i] + 1);
+			i++;
 		}
 		else
 			print_len += ft_putchar(str[i]);
